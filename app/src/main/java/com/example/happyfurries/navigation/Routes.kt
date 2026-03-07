68993453263e717b2f30de35d3a9699b288c1e8d
Ucnobia <mariam.2596@gmail.com>
2026-03-07 09:40:27 +0100

    Add CalendarScreen, routes

---
+++ app/src/main/java/com/example/happyfurries/navigation/Routes.kt
@@ -0,0 +1,8 @@
+package com.example.happyfurries.navigation
+
+object Routes {
+    const val SPLASH = "splash"
+    const val WELCOME = "welcome"
+    const val PET_FORM = "pet_form"
+    const val CALENDAR = "calendar"
+}
